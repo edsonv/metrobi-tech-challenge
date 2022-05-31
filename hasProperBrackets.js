5297580fc29339fc094c0cb75c4c0cbe35ce0778
Edson J Vargas <edson.jhair@yahoo.com>
2022-05-31 13:56:43 -0500

    Implement array tracking approach

--- hasProperBrackets.js
+++ hasProperBrackets.js
@@ -1,11 +1,30 @@
 function hasProperBrackets(text) {
-  // if (!text) return false;
-  let t = text;
-  let validator = '';
-  for (; (validator = t.replace(/\[\]|\{\}|\(\)/, '')) !== t; t = validator);
-  return !validator;
+  let brackets = {
+    '{': '}',
+    '(': ')',
+    '[': ']',
+  };
+  let verified = [];
+
+  for (let char in text) {
+    /*
+      - If the character is defined in brackets object then push it to verified array
+      - Else if the character is defined as the value of a property inside brackets object and if the first element in the verified array is not equal to the current charracter, then return false inmediately.
+    */
+    if (brackets.hasOwnProperty(text[char])) {
+      verified.push(brackets[text[char]]);
+    } else if (
+      Object.values(brackets).includes(text[char]) &&
+      verified.pop() !== text[char]
+    ) {
+      return false;
+    }
+  }
+
+  return verified.length === 0;
 }
 
-console.log(hasProperBrackets('{[]}'));
-console.log(hasProperBrackets('{(])}'));
-console.log(hasProperBrackets('{([)]}'));
+console.log(hasProperBrackets('{[]}')); // true
+console.log(hasProperBrackets('{(])}')); // false
+console.log(hasProperBrackets('{([)]}')); // false
+console.log(hasProperBrackets('{a}')); // true
