ebf7cb04764f01eae0c24d4f716893b72d6f8590
Edson J Vargas <edson.jhair@yahoo.com>
2022-05-31 16:43:06 -0500

    Implement second version

--- asyncPrint.js
+++ asyncPrint.js
@@ -1,6 +1,26 @@
 // 2) Write an async javascript function that writes every item in any given array with 1, 2, 4, 8, ..., seconds apart. ex: for [“a”,” b, “c, “d”], “a” is printed in 1 sec, “b” is printed in 2 seconds, “c” is printed in 4 seconds, ...
 
-Array.prototype.asyncPrint = async function () {
+Array.prototype.asyncPrint = function () {
+  let arr = this;
+  let timeout = 1000;
+
+  console.log(new Date());
+
+  arr.forEach((element) => {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(element);
+      }, timeout);
+    });
+
+    promise.then((resolved) => {
+      console.log(resolved, new Date());
+    });
+    timeout *= 2;
+  });
+};
+
+Array.prototype.asyncPrintV2 = async function () {
   const arr = this;
   let offset = 1000;
   let i = 0;
@@ -16,3 +36,6 @@ Array.prototype.asyncPrint = async function () {
 };
 
 ['a', 'b', 'c', 'd'].asyncPrint();
+console.log(Array.prototype.asyncPrint);
+['a', 'b', 'c', 'd'].asyncPrintV2();
+console.log(Array.prototype.asyncPrintV2);
